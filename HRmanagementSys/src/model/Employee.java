a79b07be1a35195181796e0f8c2dbea0fc834773
unknown <akshathapatil567@gmail.com>
2022-09-29 21:30:32 -0400

    First Commit

---
+++ HRmanagementSys/src/model/Employee.java
@@ -0,0 +1,119 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Dell
+ */
+public class Employee {
+    private String name;
+    private String empid;
+    private String age;
+    private String gender;
+    private String startdate;
+    private String level;
+    private String teaminfo;
+    private String positiontitle;
+    private String cellphonenum;
+    private String emailid;
+    private String photo;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmpid() {
+        return empid;
+    }
+
+    public void setEmpid(String empid) {
+        this.empid = empid;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getStartdate() {
+        return startdate;
+    }
+
+    public void setStartdate(String startdate) {
+        this.startdate = startdate;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getTeaminfo() {
+        return teaminfo;
+    }
+
+    public void setTeaminfo(String teaminfo) {
+        this.teaminfo = teaminfo;
+    }
+
+    public String getPositiontitle() {
+        return positiontitle;
+    }
+
+    public void setPositiontitle(String positiontitle) {
+        this.positiontitle = positiontitle;
+    }
+
+    public String getCellphonenum() {
+        return cellphonenum;
+    }
+
+    public void setCellphonenum(String cellphonenum) {
+        this.cellphonenum = cellphonenum;
+    }
+
+    public String getEmailid() {
+        return emailid;
+    }
+
+    public void setEmailid(String emailid) {
+        this.emailid = emailid;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+    
+    @Override
+   public String toString(){
+       return name;
+       
+   }
+    
+    
+}
